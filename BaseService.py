3b8bb11473cf9d99f482e659c8d9cdec1ab001e2
DrawFun <dzf353@gmail.com>
2017-07-08 21:39:23 +0800

    add logger

--- BaseService.py
+++ BaseService.py
@@ -2,15 +2,20 @@
 # -*- encoding: utf-8 -*-
 # @Author: DZF
 
+import sys
 import json
+import logging
 
 class BaseService(object):
-	def __init__(self):
+	def __init__(self, serviceType):
+		self.type = serviceType
+		self.logger = logging.getLogger('Jubi %s Logger' % serviceType)
+		self.logger.setLevel(logging.DEBUG)
 		try:
 			f = open('conf.json', 'r')
 			conf = json.load(f)
 			self.base_url = conf['base_url'] 
 			self.api_url = conf['api_url']
-			self.restful_url = self.base_url + self.api_url + '%s'
+			self.restful_url = self.base_url + self.api_url
 		except Exception as e:
 			raise
