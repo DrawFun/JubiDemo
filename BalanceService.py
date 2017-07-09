93e87f108733b880ee6b4ca241f76df0bd657e2e
DrawFun <dzf353@gmail.com>
2017-07-09 09:52:41 +0800

    add post_balance method

--- BalanceService.py
+++ BalanceService.py
@@ -2,12 +2,37 @@
 # -*- encoding: utf-8 -*-
 # @Author: DZF
 
-import BaseService
 import requests
 import logging
+import json
+import BaseService
+import Utils
 
 class BalanceService(BaseService.BaseService):
 	POST_BALANCE = 'balance'
 
 	def __init__(self):
 		super(BalanceService, self).__init__('Balance')
+		try:
+			f = open('keys.json', 'r')
+			conf = json.load(f)
+			self.public_key = conf['query']['public_key'] 
+			self.private_key = conf['query']['private_key']
+		except Exception as e:
+			raise
+
+	def post_balance(self):
+		nonce = Utils.generate_nonce()
+		original_params = {'nonce' : nonce, 'key' : self.public_key}
+		params = Utils.reformat_params(original_params, self.private_key)
+		print params
+		url = self.restful_url + BalanceService.POST_BALANCE
+		self.logger.info('post_balance: %s' % url)
+		response = requests.post(url, data = params)
+		return response.content if response else ''			
+
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.DEBUG)
+	balanceService = BalanceService()
+	balance = balanceService.post_balance()
+	print balance
